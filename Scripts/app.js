7c6a19a2809d8876a31c8aa974a7cbe4aa3055a1
Ahmed SGad <Ahmed.SGad@mansourgroup.com>
2021-10-24 11:51:29 +0200

    LastUpdate-0.0.1v

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,10 +1,16 @@
 let arrow  = document.querySelectorAll(".arrow");
+let nav = document.querySelector(".nav");
+let bars = document.querySelector(".fa-bars");
 
 for (let i = 0; i < arrow.length; i++) {
     arrow[i].addEventListener("click", (e)=>{
        
         let arrowParent = e.target.parentElement.parentElement;
-        console.log(arrowParent);
+        console.log(arrowParent)
         arrowParent.classList.toggle("show");
     });
 }
+bars.addEventListener("click", ()=>{
+    nav.classList.toggle('close')
+})
+
